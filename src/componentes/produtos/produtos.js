dda55b850529fe28adcdc55dc0171ef95b2fdbb7
Hellen Ferreira <contatohellenmarina@gmail.com>
2021-09-30 19:31:00 -0300

    Primeiras alterações page Home

---
+++ src/componentes/produtos/produtos.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+//  class ProductCard extends React.Component {
+//     render() {
+//       const stateProdutos = this.props.produtos
+//       return <div>
+//        {produtos.imagemURL}
+//         <div>
+//           <p>{produtos.name}</p>
+//           <p>R${produtos.value},00</p>
+ 
+//         </div>
+//       </div>
+//     }
+//   }
+
+//   export default ProductCard;
